77009e8e5900393ae763e214f4bc67e9157043bb
Saikat Rahman Asif <106202046+Asif091@users.noreply.github.com>
2025-09-08 16:26:24 +0900

    Update login-register.php

--- login-register.php
+++ login-register.php
@@ -2,7 +2,7 @@
 
 session_start();
 require_once 'db_connect.php';
-//echo "Connected successfully";
+
 
 if (isset($_POST['register'])) {
     $first_name = $_POST['first_name'];
@@ -11,7 +11,7 @@ if (isset($_POST['register'])) {
     $phone = $_POST['phone'] ?? null;
     $password = $_POST['password']; 
 
-    //$role = $_POST['role'];
+    
 
     $checkEmail = $conn->query("SELECT email FROM users WHERE email = '$email'");
     if ($checkEmail->num_rows > 0) {
@@ -32,23 +32,9 @@ if (isset($_POST['register'])) {
     header("Location: index.php");
     exit();
     
-    // $user_id = $conn->insert_id;
 
-    // if ($role === 'customer') {
-    //     $customer_type = $_POST['customer_type'] ?? 'general';
-    //     $conn->query("INSERT INTO customer (user_id, customer_type) 
-    //     VALUES ($user_id, '$customer_type')");
-    // } elseif ($role === 'admin') {
-    //     $joining_date = date('Y-m-d');
-    //     $salary = $_POST['salary'] ?? 0;
-    //     $address = $_POST['address'] ?? '';
-    //     $conn->query("INSERT INTO admin (user_id, joining_date, salary, address) 
-    //     VALUES ($user_id, '$joining_date', $salary, '$address')");
-    // }
-    // header("Location: index.php");
-    // exit();
 }
-//echo "here";
+
 if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -77,5 +63,6 @@ if (isset($_POST['login'])) {
     header("Location: index.php");
     exit();
 }
-//echo "here2";
+
+
 ?> 
